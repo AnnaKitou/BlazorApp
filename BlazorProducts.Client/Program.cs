aa24defae7af89aa0753b3d4a2e06daa24af5979
AnnaKitou <93220476+AnnaKitou@users.noreply.github.com>
2022-05-10 22:16:30 +0300

    Getting Products

--- BlazorProducts.Client/Program.cs
+++ BlazorProducts.Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazorProducts.Client.HttpRepository;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,16 +11,26 @@ using System.Threading.Tasks;
 
 namespace BlazorProducts.Client
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
 
-            await builder.Build().RunAsync();
-        }
-    }
+			builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+			builder.Services.AddHttpClient("ProductsAPI", cl =>
+			{
+				cl.BaseAddress = new Uri("https://localhost:5011/api/");
+			});
+
+			builder.Services.AddScoped(
+				sp => sp.GetService<IHttpClientFactory>().CreateClient("ProductsAPI"));
+
+			builder.Services.AddScoped<IProductHttpRepository, ProductHttpRepository>();
+
+			await builder.Build().RunAsync();
+		}
+	}
 }
