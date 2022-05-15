c10edc1968ddcb331f7da0897a823bf15e49481e
AnnaKitou <93220476+AnnaKitou@users.noreply.github.com>
2022-05-15 18:15:30 +0300

    Pagination - Searching - Sorting

---
+++ BlazorProducts.Client/Features/PagingLink.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorProducts.Client.Features
+{
+    public class PagingLink
+    {
+        public PagingLink(string text, int page, bool enabled)
+        {
+            Text = text;
+            Page = page;
+            Enabled = enabled;
+        }
+
+        public string Text { get; set; }
+        public int Page { get; set; }
+        public bool Active { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
--- BlazorProducts.Client/Pages/Products.razor.cs
+++ BlazorProducts.Client/Pages/Products.razor.cs
@@ -1,5 +1,6 @@
 ﻿using BlazorProducts.Client.HttpRepository;
 using Entities.Models;
+using Entities.RequestFeatures;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
@@ -8,21 +9,57 @@ using System.Threading.Tasks;
 
 namespace BlazorProducts.Client.Pages
 {
-	public partial class Products
-	{
-		public List<Product> ProductList { get; set; } = new List<Product>();
+    public partial class Products
+    {
+        public List<Product> ProductList { get; set; } = new List<Product>();
+        public MetaData MetaData { get; set; } = new MetaData();
 
-		[Inject]
-		public IProductHttpRepository ProductRepo { get; set; }
+        private ProductParameters _productParameters = new ProductParameters();
 
-		protected async override Task OnInitializedAsync()
-		{
-			ProductList = await ProductRepo.GetProducts();
+        [Inject]
+        public IProductHttpRepository ProductRepo { get; set; }
 
-			foreach (var product in ProductList)
-			{
-				Console.WriteLine(product.Name);
-			}
-		}
-	}
+        protected async override Task OnInitializedAsync()
+        {
+            await GetProducts();
+
+        }
+
+        private async Task SelectedPage(int page)
+        {
+            _productParameters.PageNumber = page;
+            await GetProducts();
+        }
+
+        private async Task GetProducts()
+        {
+            var pagingResponse = await ProductRepo.GetProducts(_productParameters);
+
+            ProductList = pagingResponse.Items;
+            MetaData = pagingResponse.MetaData;
+        }
+
+        private async Task SetPageSize(int pageSize)
+        {
+            _productParameters.PageSize = pageSize;
+            _productParameters.PageNumber = 1;
+
+            await GetProducts();
+        }
+
+        private async Task SearchChanged(string searchTerm)
+        {
+            _productParameters.PageNumber = 1;
+            _productParameters.SearchTerm = searchTerm;
+
+            await GetProducts();
+        }
+
+        private async Task SortChanged(string orderBy)
+        {
+            _productParameters.OrderBy = orderBy;
+
+            await GetProducts();
+        }
+    }
 }
