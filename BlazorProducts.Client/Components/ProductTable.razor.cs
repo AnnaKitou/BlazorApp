3250b0f0a54db6000b4040810f492702459ecd6e
AnnaKitou <93220476+AnnaKitou@users.noreply.github.com>
2022-05-22 23:09:14 +0300

    Delete Product Complete

--- BlazorProducts.Client/Components/ProductTable.razor.cs
+++ BlazorProducts.Client/Components/ProductTable.razor.cs
@@ -1,7 +1,9 @@
-﻿using Entities.Models;
+﻿using BlazorProducts.Client.Shared;
+using Entities.Models;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BlazorProducts.Client.Components
 {
@@ -12,5 +14,19 @@ namespace BlazorProducts.Client.Components
 
         [Parameter]
         public EventCallback<Guid> OnDelete { get; set; }
+
+        private Confirmation _confirmation;
+        private Guid _productToDelete;
+        private void CallConfirmationModal(Guid id)
+        {
+            _productToDelete = id;
+            _confirmation.Show();
+        }
+
+        private async Task DeleteProduct()
+        {
+            _confirmation.Hide();
+            await OnDelete.InvokeAsync(_productToDelete);
+        }
     }
 }
