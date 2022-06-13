8c0c19ad71a7b9e72719237a2ed85628218c28f0
AnnaKitou <93220476+AnnaKitou@users.noreply.github.com>
2022-06-13 22:32:47 +0300

    Publish

--- BlazorProducts.Server/BlazorProducts.Server/Startup.cs
+++ BlazorProducts.Server/BlazorProducts.Server/Startup.cs
@@ -38,6 +38,10 @@ namespace BlazorProducts.Server
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllers();
+
+            services.AddRazorPages();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,11 +50,13 @@ namespace BlazorProducts.Server
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
             }
 
             app.UseHttpsRedirection();
             app.UseCors("CorsPolicy");
-
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),
@@ -65,6 +71,8 @@ namespace BlazorProducts.Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapRazorPages();
+                endpoints.MapFallbackToFile("index.html");
             });
         }
     }
